cb0239e20ae7286c7d770a2fe269a7909aab59e6
noblegascoin <46994883+noblegascoin@users.noreply.github.com>
2019-01-25 14:35:42 +0900

    Change path

--- src/util.cpp
+++ src/util.cpp
@@ -559,7 +559,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "monacoin";
+    const char* pszModule = "noblegascoin";
 #endif
     if (pex)
         return strprintf(
@@ -584,7 +584,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Monacoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "NobleGasCoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -594,10 +594,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Monacoin";
+    return pathRet / "Library/Application Support/NobleGasCoin";
 #else
     // Unix
-    return pathRet / ".monacoin";
+    return pathRet / ".noblegascoin";
 #endif
 #endif
 }
